ee4843e91341034ba59625e9468a0e6c40d7156f
luckydonald <luckydonald@flutterb.at>
2015-09-16 01:50:12 +0200

    Fixed coloring in webserver. This is v0.2 now.

--- shairportdecoder/__init__.py
+++ shairportdecoder/__init__.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-from shairportdecoder.metadata import Infos, Item, logger, CoverArt
-from shairportdecoder.decoder import logger
-
 __author__ = 'luckydonald'
 VERSION = "0.2"
 
